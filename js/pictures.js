4f176f1f6005988ffd7b7a17abf328bab8e0e293
AniLiss <anatskayalisa@gmail.com>
2017-02-25 15:17:57 +0300

    subtask 7

--- js/pictures.js
+++ js/pictures.js
@@ -8,10 +8,21 @@ var onLoad = (function () {
     var templateElement = document.querySelector('#picture-template');
     var elementToClone = templateElement.content.querySelector('.picture');
 
-    var showGalleryCb = function (url, comments, likes) {
+    var showGalleryCb = function (url, likes, comments) {
       return function () {
-        window.showGallery(url, comments, likes);
+        window.showGallery(url, likes, comments);
       };
+    };
+
+    var showGalleryItem = function (e) {
+      // window.console.log(e.target.parentNode);
+      // newEl.addEventListener('click', showGalleryCb(url, comments, likes));
+      var url = e.target.parentNode.querySelector('img').src;
+      var comment = e.target.parentNode.querySelector('.picture-comments').innerText;
+      var likes = e.target.parentNode.querySelector('.picture-likes').innerText;
+      window.console.log(likes);
+      // showGalleryCb(url, likes, comment);
+      window.showGallery(url, likes, comment);
     };
 
     var renderPicture = function (elemToCln, picCont) {
@@ -22,10 +33,15 @@ var onLoad = (function () {
         newElement.querySelector('.picture-comments').innerText = object.comments;
         newElement.querySelector('.picture-likes').innerText = object.likes;
         picCont.appendChild(newElement);
-        newElement.addEventListener('click', showGalleryCb(object.url, object.comments, object.likes));
+        // showGalleryItem(newElement, object.url, object.comments, object.likes);
+        // newElement.addEventListener('click', showGalleryCb(object.url, object.comments, object.likes));
       });
+      picCont.addEventListener('click', showGalleryItem);
+
     };
     renderPicture(elementToClone, picturesContainer);
+
+    // picturesContainer.addEventListener('click', showGalleryItem);
   };
 })();
 
