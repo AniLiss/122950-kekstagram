18aeb4f28b1f78da900443bc022a5a0bd6b216b8
AniLiss <anatskayalisa@gmail.com>
2017-02-25 20:13:46 +0300

    tasks 1-8

--- js/pictures.js
+++ js/pictures.js
@@ -1,48 +1,74 @@
 'use strict';
-var DATA_URL = 'https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
-var picturesList = [];
-var onLoad = (function () {
-  return function (evt) {
-    var data = JSON.parse(evt.target.response);
+(function () {
+  var DATA_URL = 'https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
+  var picturesList = [];
+
+  var prapareGalleryItemToShow = function (e) {
+    e.preventDefault();
+    var url = e.target.parentNode.querySelector('img').src;
+    var comment = e.target.parentNode.querySelector('.picture-comments').innerText;
+    var likes = e.target.parentNode.querySelector('.picture-likes').innerText;
+    window.showGalleryItem(url, likes, comment);
+  };
+
+  var renderPictures = function (pictures) {
     var picturesContainer = document.querySelector('.pictures');
     var templateElement = document.querySelector('#picture-template');
     var elementToClone = templateElement.content.querySelector('.picture');
 
-    // var showGalleryCb = function (url, likes, comments) {
-    //   return function () {
-    //     window.showGallery(url, likes, comments);
-    //   };
-    // };
-
-    var showGalleryItem = function (e) {
-      // window.console.log(e.target.parentNode);
-      // newEl.addEventListener('click', showGalleryCb(url, comments, likes));
-      var url = e.target.parentNode.querySelector('img').src;
-      var comment = e.target.parentNode.querySelector('.picture-comments').innerText;
-      var likes = e.target.parentNode.querySelector('.picture-likes').innerText;
-      window.console.log(likes);
-      // showGalleryCb(url, likes, comment);
-      window.showGallery(url, likes, comment);
+    var renderPicture = function (dataObject) {
+      var newElement = elementToClone.cloneNode(true);
+      newElement.querySelector('img').src = dataObject.url;
+      newElement.querySelector('.picture-comments').innerText = dataObject.comments.length;
+      newElement.querySelector('.picture-likes').innerText = dataObject.likes;
+      picturesContainer.appendChild(newElement);
     };
 
-    var renderPicture = function (elemToCln, picCont) {
-      data.forEach(function (object) {
-        picturesList.push(object);
-        var newElement = elemToCln.cloneNode(true);
-        newElement.querySelector('img').src = object.url;
-        newElement.querySelector('.picture-comments').innerText = object.comments;
-        newElement.querySelector('.picture-likes').innerText = object.likes;
-        picCont.appendChild(newElement);
-        // showGalleryItem(newElement, object.url, object.comments, object.likes);
-        // newElement.addEventListener('click', showGalleryCb(object.url, object.comments, object.likes));
-      });
-      picCont.addEventListener('click', showGalleryItem);
+    pictures.forEach(function (object) {
+      renderPicture(object);
+    });
 
-    };
-    renderPicture(elementToClone, picturesContainer);
-
-    // picturesContainer.addEventListener('click', showGalleryItem);
+    picturesContainer.addEventListener('click', prapareGalleryItemToShow);
   };
-})();
 
-window.load(DATA_URL, onLoad);
+  var onLoad = (function () {
+    return function (evt) {
+      var data = JSON.parse(evt.target.response);
+      picturesList = data;
+      renderPictures(picturesList);
+    };
+  })();
+
+  window.load(DATA_URL, onLoad);
+}());
+
+// var onLoad = (function () {
+//   return function (evt) {
+//     var data = JSON.parse(evt.target.response);
+//     var picturesContainer = document.querySelector('.pictures');
+//     var templateElement = document.querySelector('#picture-template');
+//     var elementToClone = templateElement.content.querySelector('.picture');
+//
+//     var showGalleryItem = function (e) {
+//       e.preventDefault();
+//       var url = e.target.parentNode.querySelector('img').src;
+//       var comment = e.target.parentNode.querySelector('.picture-comments').innerText;
+//       var likes = e.target.parentNode.querySelector('.picture-likes').innerText;
+//       window.console.log(likes);
+//       window.showGallery(url, likes, comment);
+//     };
+//
+//     var renderPicture = function (elemToCln, picCont) {
+//       data.forEach(function (object) {
+//         var newElement = elemToCln.cloneNode(true);
+//         newElement.querySelector('img').src = object.url;
+//         newElement.querySelector('.picture-comments').innerText = object.comments.length;
+//         newElement.querySelector('.picture-likes').innerText = object.likes;
+//         picCont.appendChild(newElement); });
+//       picturesList = data;
+//       picCont.addEventListener('click', showGalleryItem);
+//
+//     };
+//     renderPicture(elementToClone, picturesContainer);
+//   };
+// })();
--- js/show-gallery.js
+++ js/show-gallery.js
@@ -1,14 +1,22 @@
 'use strict';
-window.showGallery = function (url, likes, comments) {
+window.showGalleryItem = function (url, likes, comments) {
   var galleryOverlay = document.querySelector('.gallery-overlay');
-  // var closeGalleryBtn = galleryOverlay.querySelector('.gallery-overlay-close');
+  var closeGalleryBtn = galleryOverlay.querySelector('.gallery-overlay-close');
+
   galleryOverlay.classList.remove('invisible');
   galleryOverlay.querySelector('.gallery-overlay-image').src = url;
   galleryOverlay.querySelector('.gallery-overlay-controls-like').innerText = likes;
   galleryOverlay.querySelector('.gallery-overlay-controls-comments').innerText = comments;
 
-  // var closeGalleryOverlay = function () {
-  //   galleryOverlay.classList.add('invisible');
-  // };
-  // closeGalleryBtn.addEventListener('click', closeGalleryOverlay);
+  var closeGalleryOverlay = function (galleryOverlayData) {
+    return function (e) {
+      // window.closeGalleryOverlayCb(galleryOverlayData);
+      if (e.keyCode === window.ESC_KEY_CODE || e.keyCode === window.ENTER_KEY_CODE || e.which === 1) {
+        window.closeGalleryOverlayCb(galleryOverlayData);
+      }
+    };
+  };
+  closeGalleryBtn.addEventListener('click', closeGalleryOverlay(galleryOverlay));
+  closeGalleryBtn.addEventListener('keydown', closeGalleryOverlay(galleryOverlay));
+  galleryOverlay.addEventListener('keydown', closeGalleryOverlay(galleryOverlay));
 };
