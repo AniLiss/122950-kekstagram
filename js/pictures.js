ff2c695d6f5db155588fb0de917784e19daf86ac
AniLiss <anatskayalisa@gmail.com>
2017-02-26 02:36:05 +0300

    filters' change without clearance

--- js/pictures.js
+++ js/pictures.js
@@ -31,11 +31,62 @@
     picturesContainer.addEventListener('click', prapareGalleryItemToShow);
   };
 
+  var checkElementExistence = function (randomPictures, currentPicture) {
+    for (var j = 0; j < randomPictures.length; j++) {
+      if (randomPictures[j] === currentPicture) {
+        return false;
+      }
+    }
+    return randomPictures.push(currentPicture);
+  };
+
+  var createRandomPictureList = function (randomPictures, currentPicturesList) {
+    while (randomPictures.length < 10) {
+      var randomIndex = Math.floor(Math.random() * (currentPicturesList.length));
+      var currentPicture = currentPicturesList[randomIndex];
+      checkElementExistence(randomPictures, currentPicture);
+    }
+  };
+
+  var sortPhotos = (function () {
+    return function (e) {
+      var filterValue = e.target.htmlFor;
+      var randomPictures = [picturesList[0]];
+      if (filterValue === 'filter-popular') {
+        renderPictures(picturesList);
+      } else if (filterValue === 'filter-new') {
+        createRandomPictureList(randomPictures, picturesList);
+        renderPictures(randomPictures);
+      } else if (filterValue === 'filter-discussed') {
+        var sortedByCommentNumber = picturesList;
+        for (var i = 0; i < picturesList.length; i++) {
+          sortedByCommentNumber.sort(function (a, b) {
+            if (a.comments.length > b.comments.length) {
+              return 1;
+            }
+            if (a.comments.length < b.comments.length) {
+              return -1;
+            }
+            return 0;
+          });
+        }
+        renderPictures(sortedByCommentNumber);
+      }
+    };
+  })();
+
+  var filterPhotos = function () {
+    var filters = document.querySelector('.filters');
+    filters.classList.remove('hidden');
+    filters.addEventListener('click', sortPhotos);
+  };
+
   var onLoad = (function () {
     return function (evt) {
       var data = JSON.parse(evt.target.response);
       picturesList = data;
       renderPictures(picturesList);
+      filterPhotos();
     };
   })();
 
